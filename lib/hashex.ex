25008c4316393502cbfe3b1f7d192b800dc1619d
tim <llStrainll@gmail.com>
2014-08-19 23:41:15 +0400

    some fixes

--- lib/hashex.ex
+++ lib/hashex.ex
@@ -22,7 +22,7 @@ defprotocol HashUtils do
   def get( hash, lst )
   def set( hash, lst, val )
   def modify( hash, lst, func )
-  def modify_each( hash, lst, func )
+  def modify_all( hash, lst, func )
   def delete( hash, lst )
 end
 
@@ -47,7 +47,7 @@ defimpl HashUtils, for: Map do
   end
 
   # modify each field of hash, except :__struct__
-  def modify_each( hash, [], func ) do
+  def modify_all( hash, [], func ) do
     Enum.reduce( Map.to_list(hash), hash, fn({k, v}, res) ->
       case k do
         :__struct__ -> Map.update!( res, k, fn(_) -> v end )
@@ -55,8 +55,8 @@ defimpl HashUtils, for: Map do
       end
     end )
   end
-  def modify_each( hash, [key|rest], func ) do
-    Map.update!( hash, key, fn(_) -> HashUtils.modify_each(Map.get( hash, key ) , rest, func) end )
+  def modify_all( hash, [key|rest], func ) do
+    Map.update!( hash, key, fn(_) -> HashUtils.modify_all(Map.get( hash, key ) , rest, func) end )
   end
 
   def delete( hash, [key|[]] ) do
@@ -92,7 +92,7 @@ defimpl HashUtils, for: List do
   end
 
   # modify each field of hash if hash is_keylist, or just do Enum.map for this element
-  def modify_each( lst, [], func ) do
+  def modify_all( lst, [], func ) do
     case is_keylist( lst ) do
       true -> Enum.reduce( lst, lst, fn({k, _}, res) ->
                 Dict.update!( res, k, func )
@@ -100,8 +100,8 @@ defimpl HashUtils, for: List do
       false -> Enum.map( lst, func )
     end
   end
-  def modify_each( hash, [key | rest], func ) do
-    Dict.update!( hash, key, fn(_) -> HashUtils.modify_each(Dict.get( hash, key ) , rest, func) end )
+  def modify_all( hash, [key | rest], func ) do
+    Dict.update!( hash, key, fn(_) -> HashUtils.modify_all(Dict.get( hash, key ) , rest, func) end )
   end
 
   def delete( hash, [key|[]] ) do
@@ -111,7 +111,7 @@ defimpl HashUtils, for: List do
     Dict.update!( hash, key, fn(_) -> HashUtils.delete(Dict.get( hash, key ) , rest) end )
   end
 
-  # priv funcs for modify_each
+  # priv funcs for modify_all
   defp is_keylist lst do
     is_lst_of_tuples(lst)
       |> is_tuples_size2(lst)
